4bfe1e83558c899fb5a5ead520a997f2faadb058
Blisss712 <derekjoshreyes132@gmail.com>
2025-12-09 21:40:18 +0800

    added remove from cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -38,7 +38,7 @@ cart.forEach(cartItem => {
                     <span class="update-quantity-link link-primary">
                         Update
                     </span>
-                    <span class="delete-quantity-link link-primary">
+                    <span class="delete-quantity-link link-primary js-delete-quantity-link" data-product-id="${matchingProduct.id}">
                         Delete
                     </span>
                 </div>
@@ -87,3 +87,10 @@ cart.forEach(cartItem => {
 })
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML
+
+document.querySelectorAll(".js-delete-quantity-link").forEach((link) => {
+    link.addEventListener("click", () => {
+        const productId = link.dataset.productId
+        console.log(productId)
+    })
+})
