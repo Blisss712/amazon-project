769195ace07af0238e8b3f664365678ac9103d88
Blisss712 <derekjoshreyes132@gmail.com>
2025-12-13 05:31:57 +0800

    used callback and done() for backend testing in jasmine

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,13 +1,22 @@
 import {renderOrderSummary} from "../../scripts/checkout/orderSummary.js";
 import {addToCart, cart, loadFromStorage} from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe('test suite: renderOrderSummary', () => {
-    it('displays the cart', () => {
+    const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6"
+    const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d"
+    beforeAll((done) => {
+        loadProducts(() => {
+            done()
+        })
+    })
+
+    beforeEach(() => {
+        spyOn(localStorage, 'setItem') 
         document.querySelector('.js-test-container').innerHTML = `
             <div class="js-order-summary"></div>
+            <div class="js-payment-summary"></div>
         `
-        const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6"
-        const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d"
 
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([
@@ -25,8 +34,10 @@ describe('test suite: renderOrderSummary', () => {
         })
 
         loadFromStorage()   
-        renderOrderSummary()
-        
+        renderOrderSummary()   
+    })   
+
+    it('displays the cart', () => {
         expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(2)
         expect(document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain('Quantity: 2')
         expect(document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1')
@@ -34,32 +45,6 @@ describe('test suite: renderOrderSummary', () => {
     })
 
     it('removes a product from the cart', () => {
-        spyOn(localStorage, 'setItem') 
-        document.querySelector('.js-test-container').innerHTML = `
-            <div class="js-order-summary"></div>
-            <div class="js-payment-summary"></div>
-        `
-        const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6"
-        const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d"
-
-        spyOn(localStorage, 'getItem').and.callFake(() => {
-            return JSON.stringify([
-                {
-                    productId: productId1,
-                    quantity: 2,
-                    deliveryOptionId: '1'
-                },
-                {
-                    productId: productId2,
-                    quantity: 1,
-                    deliveryOptionId: '2'
-                }
-            ])
-        })
-
-        loadFromStorage()   
-        renderOrderSummary()
-
         document.querySelector(`.js-delete-quantity-link-${productId1}`).click()
 
 
