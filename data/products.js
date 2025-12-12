ab07ec67aed0ab0bbbe58d6fe49bd08ea69f16f2
Blisss712 <derekjoshreyes132@gmail.com>
2025-12-13 04:22:30 +0800

    backend practice

--- data/products.js
+++ data/products.js
@@ -56,23 +56,11 @@ class Clothing extends Product {
   }
 }
 
-const product1 = new Product({
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-  keywords: [
-    "socks",
-    "sports",
-    "apparel"
-  ]
-  })
-
-console.log(product1)
+/*
+const date = new Date()
+console.log(date)
+console.log(date.toLocaleTimeString())
+*/
 
 export const products = [
   {
